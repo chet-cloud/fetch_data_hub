3fc6892d4a3b4357eb3ccce554c8af0fdfe05823
cchen@artisreit.com <azuredevuser@AZ-Dev-01.fadhrw2s14yezgdft3zvbnq3tb.bx.internal.cloudapp.net>
2022-02-03 22:54:41 +0000

    add node schedule and organise the index.js

--- index.js
+++ index.js
@@ -3,61 +3,13 @@
 const express = require('express');
 const path = require('path');
 const { createServer } = require('http');
-const subscribes = require('./lib/subscribes');
-const WebSocket = require('ws')
-
+const startWebSocketServer  = require('./lib/websocket');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 const server = createServer(app);
-const wss = new WebSocket.Server({server: server, clientTracking: true, noServer: false});
+startWebSocketServer(server)
 
-
-const interval = setInterval(function ping() {
-    wss.clients.forEach(function each(ws) {
-        if (ws.isAlive === false) {
-            return ws.terminate();
-        }
-        ws.isAlive = false;
-        if (ws['isBrowser']) {
-            ws.send("ping")
-        } else {
-            ws.ping();
-        }
-    });
-}, 30000);
-
-wss.on('close', function close() {
-    clearInterval(interval);
-});
-
-
-wss.on('connection', function (ws, request) {
-    ws.isAlive = true;
-    ws.on('pong', () => {
-        ws.isAlive = true;
-    });
-
-    ws.send("->: Hi")
-
-    ws.on('message', function (message) {
-        if (message === "pong") {
-            ws.isAlive = true;
-            ws['isBrowser'] = true
-        }
-        subscribes(wss, ws, message)
-        console.log(`Received message ${message} `);
-    });
-
-    ws.on('close', function () {
-        console.log(`close ${ws}`);
-    });
-
-});
-
-//
-// Start the server.
-//
 server.listen(8080, function () {
     console.log('Listening on http://localhost:8080');
 });
---
+++ test.js
@@ -0,0 +1,15 @@
+// *    *    *    *    *    *
+// ┬    ┬    ┬    ┬    ┬    ┬
+// │    │    │    │    │    │
+// │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+// │    │    │    │    └───── month (1 - 12)
+// │    │    │    └────────── day of month (1 - 31)
+// │    │    └─────────────── hour (0 - 23)
+// │    └──────────────────── minute (0 - 59)
+// └───────────────────────── second (0 - 59, OPTIONAL)
+
+
+const schedule = require('node-schedule');
+const job = schedule.scheduleJob('*/3 * * * * *', function(fireDate){
+    console.log('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
+});
