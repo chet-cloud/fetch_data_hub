4e860cf48c8c3946b7c752adbb41524977f68906
chet-hub <gmail@gmail.com>
2022-02-03 21:19:43 -0600

    save work

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 const express = require('express');
 const path = require('path');
 const { createServer } = require('http');
-const startWebSocketServer  = require('./lib/websocket');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 const server = createServer(app);
-startWebSocketServer(server)
+require('./lib/websocket')(server)
 
 server.listen(8080, function () {
     console.log('Listening on http://localhost:8080');
